a10792c0675a9f8edb6cf505528332c66bdb98ee
azeuio <nicolas.mesrine@epitech.eu>
2022-12-03 18:43:39 +0100

    :recycle: #1 remove implicit conversions

--- src/core/window/update_view.cpp
+++ src/core/window/update_view.cpp
@@ -13,6 +13,6 @@ void Window::updateView(const sf::Event &event)
 {
     sf::View v = _display.getView();
 
-    v.setSize(sf::Vector2f(event.size.width, event.size.height));
+    v.setSize(sf::Vector2f((float)event.size.width, (float)event.size.height));
     _display.setView(v);
 }
